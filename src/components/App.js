5de871a052cf6165c6d353cc4704d2e606a8913c
Nicolas Corbalan <outreef@hotmail.com>
2018-06-04 19:22:46 +0200

    boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Welcome to my Starter App
+                </h1>
+            </div>
+        )
+    } 
+}
+
+export default App 
---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+// telling Webpack where to find the base JavaScript file. In our application, that’s index.js. Then it tells it where to output the built file when it’s done with it.
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require ('path');
+
+module.exports = {
+    entry:{
+        main: './src/index.js',
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve('./dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: ['node_modules'],
+                use: [{ loader: 'babel-loader'}],
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [{
+                    loader: 'style-loader'},
+                    {
+                    loader: 'css-loader'},
+                    {
+                    loader: 'sass-loader'
+                }],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        open: true,
+    }
+}
